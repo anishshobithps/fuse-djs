a11cba8ae02be34cfd3d08fe0329af14fe387cbf
Anish-Shobith <>
2020-12-19 16:18:25 +0000

    build: tsc build for 20ffd98ffb6231c61b337864dfc3ae48e8a19f16

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,7 +1,7 @@
-import Collection from '@discordjs/collection';
 import Fuse from 'fuse.js';
-import FuseOptions from 'fuse.js';
-export default class FuzzySearch<K extends string, V> {
+import type FuseOptions from 'fuse.js';
+import type Collection from '@discordjs/collection';
+export declare class FuzzySearch<K extends string, V> {
     private readonly collectionArray;
     constructor(collection: Collection<K, V>);
     run(query: string, options: FuseOptions<V>): Fuse.FuseResult<V>[];
--- dist/index.js
+++ dist/index.js
@@ -3,6 +3,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.FuzzySearch = void 0;
 const fuse_js_1 = __importDefault(require("fuse.js"));
 class FuzzySearch {
     constructor(collection) {
@@ -16,4 +17,4 @@ class FuzzySearch {
         return fuzzyFuse.search(locquery);
     }
 }
-exports.default = FuzzySearch;
+exports.FuzzySearch = FuzzySearch;
