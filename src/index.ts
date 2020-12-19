bdc814225cbd8f831d9ba53d4780ba81691624dd
Anish-Shobith <anish.shobith19@gmail.com>
2020-12-19 15:38:09 +0530

    feat(fuse-djs): 1.0.0

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log('Hello World!');
+import Collection from '@discordjs/collection';
+import Fuse from 'fuse.js';
+// eslint-disable-next-line no-duplicate-imports
+import FuseOptions from 'fuse.js';
+
+export default class FuzzySearch<K extends string, V> {
+  private readonly collectionArray: ReadonlyArray<V>;
+  public constructor(collection: Collection<K, V>) {
+    this.collectionArray = [...collection.values()];
+  }
+
+  public run(query: string, options: FuseOptions<V>) {
+    const locquery = query.toLowerCase();
+    const fuzzyFuse = new Fuse(this.collectionArray, {
+      ...options,
+    });
+    return fuzzyFuse.search(locquery);
+  }
+}
