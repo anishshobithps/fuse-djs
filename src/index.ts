4d27da789424aa9de6bd49a8181fb70f4995f964
Anish-Shobith <anish.shobith19@gmail.com>
2020-12-20 02:28:53 +0530

    feat(readme and changes): Rework and fix stuff

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,24 @@
 import Fuse from 'fuse.js';
-// eslint-disable-next-line no-duplicate-imports
-import type FuseOptions from 'fuse.js';
+
 import type Collection from '@discordjs/collection';
 export class FuzzySearch<K extends string, V> {
   private readonly collectionArray: ReadonlyArray<V>;
-  public constructor(collection: Collection<K, V>) {
+  private readonly fuseOptions?: Fuse.IFuseOptions<V>;
+  public constructor(
+    collection: Collection<K, V>,
+    keys: string[],
+    options?: Fuse.IFuseOptions<V>
+  ) {
     this.collectionArray = [...collection.values()];
+    this.fuseOptions = {
+      ...options,
+      keys,
+    };
   }
 
-  public run(query: string, options: FuseOptions<V>) {
+  public run(query: string) {
     const locquery = query.toLowerCase();
-    const fuzzyFuse = new Fuse(this.collectionArray, {
-      ...options,
-    });
+    const fuzzyFuse = new Fuse(this.collectionArray, this.fuseOptions);
     return fuzzyFuse.search(locquery);
   }
 }
